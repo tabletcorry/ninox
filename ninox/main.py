bd6b6269e65aa38d4863c6108488a5feb9f0b06a
Corry Haines <github@corryh.com>
2025-05-19 18:42:30 -0700

    Add git commit helper (#13)

--- ninox/main.py
+++ ninox/main.py
@@ -1,6 +1,6 @@
 import click
 
-from ninox import image_description, s3_hugo
+from ninox import git_commands, image_description, s3_hugo
 
 
 @click.group()
@@ -10,3 +10,4 @@ def cli() -> None:
 
 cli.add_command(image_description.describe_images)
 cli.add_command(s3_hugo.generate_menu_tree)
+cli.add_command(git_commands.git)
